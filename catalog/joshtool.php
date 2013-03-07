4c8717511b00bbdee3214c00538865abc813e6ae
Joshua Kossoff <kossoff.j@gmail.com>
2013-03-06 19:55:38 -0500

    Commiting joshtool changes.

--- catalog/joshtool.php
+++ catalog/joshtool.php
@@ -5,7 +5,26 @@
 
   require(DIR_WS_INCLUDES . 'template_top.php');
 
-  echo("hi!");
+?>
+<div id="carSelectContainer">
+    <label for="carMakeSelect">Select Make:</label>
+    <select id="carMakeSelect">
+      <?php
+    $car_make_query = tep_db_query("SELECT DISTINCT(cars_make) FROM trs.cars ORDER BY cars_make ASC;");
+
+while($car_make_value = tep_db_fetch_array($car_make_query)) {  
+  $make = $car_make_value["cars_make"];
+  echo("<option value=\"" . $make . "\">" . $make . "</option>");
+}
+
+      ?>
+    </select>
+    <?php
+    echo("DEBUG: dumping car_make_query: ");var_dump($car_make_query);
+echo("DEBUG: dumping car_make_value: ");var_dump($car_make_value);
+ ?>
+</div>
+<?php
 
   require(DIR_WS_INCLUDES . 'template_bottom.php');
   require(DIR_WS_INCLUDES . 'application_bottom.php');
